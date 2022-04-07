0faa7446ef6d37499a3c578bd18933667f8df39d
Sayfiddin11 <ksayfiddin4727@gmail.com>
2022-04-07 15:32:57 +0500

    BottomNavigationBar

--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -1,12 +1,43 @@
+import 'package:final_exam/constant/size_config.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 class MyHomePage extends StatelessWidget {
   const MyHomePage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    SizeConfig().init(context);
     return Scaffold(
-      
+      bottomNavigationBar: BottomNavigationBar(
+        type: BottomNavigationBarType.fixed,
+        items: <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: SvgPicture.asset(
+              "assets/images/Document.svg",
+            ),
+            label: 'Contracts',
+          ),
+          BottomNavigationBarItem(
+            icon: SvgPicture.asset("assets/images/Time Circle.svg"),
+            label: 'History',
+          ),
+          BottomNavigationBarItem(
+            icon: SvgPicture.asset("assets/images/Plus.svg"),
+            label: 'New',
+          ),
+          BottomNavigationBarItem(
+            icon: SvgPicture.asset("assets/images/Bookmark.svg"),
+            label: 'Saved',
+          ),
+          BottomNavigationBarItem(
+            icon: SvgPicture.asset(
+              "assets/images/Profile.svg",
+            ),
+            label: 'Profile',
+          ),
+        ],
+      ),
     );
   }
 }
